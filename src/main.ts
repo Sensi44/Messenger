7abae892d5fbccc00626c6174ffe1e2e1084a128
Classified <mur.sensi@yandex.ru>
2024-08-04 23:18:41 +0300

    learn handlebars

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import HandleBars from 'handlebars';
 import * as Components from './components';
 import * as Pages from './pages';
+import cat1 from './assets/img/1.png';
+import cat2 from './assets/img/2.png';
+import cat3 from './assets/img/3.png';
 
 const pages = {
   'login': [Pages.LoginPage, {test: '123'}],
-  'list': [Pages.ListPage],
+  'list': [Pages.ListPage, { cats: [
+      { name: 'cat-1', avatar: cat1 },
+      { name: 'cat-2', avatar: cat2, active: true },
+      { name: 'cat-3', avatar: cat3 },
+    ]}],
   'nav': [Pages.NavigatePage],
 }
 
