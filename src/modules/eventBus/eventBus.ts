7a4edfbc4493a29463625dfb080c6a35148b7053
Classified <mur.sensi@yandex.ru>
2024-09-01 15:45:08 +0300

    ts fix

--- src/modules/eventBus/eventBus.ts
+++ src/modules/eventBus/eventBus.ts
@@ -1,4 +1,4 @@
-export type Listener<T extends unknown[] = any[]> = (...args: T) => void;
+export type Listener<T extends unknown[] = unknown[]> = (...args: T) => void;
 
 export default class EventBus<E extends string = string, M extends { [K in E]: unknown[] } = Record<E, any[]>> {
   private listeners: { [key in E]?: Listener<M[E]>[] } = {};
@@ -16,9 +16,7 @@ export default class EventBus<E extends string = string, M extends { [K in E]: u
       throw new Error(`Нет события: ${event}`);
     }
 
-    this.listeners[event] = this.listeners[event]!.filter(
-      listener => listener !== callback,
-    );
+    this.listeners[event] = this.listeners[event]!.filter((listener) => listener !== callback);
   }
 
   emit(event: E, ...args: M[E]) {
