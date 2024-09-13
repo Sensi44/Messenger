115b106ff58555568a62f03f4eaa9c5833001c3e
Classified <mur.sensi@yandex.ru>
2024-09-14 01:07:31 +0300

    start work with xhr

---
+++ src/api/temp.ts
@@ -0,0 +1,47 @@
+import { HTTPTransport } from '../modules/xhr/myFetch.ts';
+import { ICreateUser, ISignInUser, IUpdateUserAvatar, IUpdateUserInfo, IUpdateUserPassword } from '../types/user';
+
+export class UserAPI extends HTTPTransport {
+  static async create(values: ICreateUser) {
+    const res = await this.post('/auth/signup', { data: values });
+    return this.checkResponse(res);
+  }
+
+  static async updateInfo(values: IUpdateUserInfo) {
+    const res = await this.put('/user/profile', { data: values });
+    return this.checkResponse(res);
+  }
+
+  static async updatePassword(values: IUpdateUserPassword) {
+    const res = await this.put('/user/password', { data: values });
+    return this.checkResponse(res);
+  }
+
+  static async updateAvatar(value: IUpdateUserAvatar) {
+    const formData = new FormData();
+    formData.append('avatar', value.avatar);
+
+    const res = await this.put('/user/profile/avatar', { data: formData });
+    return this.checkResponse(res);
+  }
+
+  static async signIn(values: ISignInUser) {
+    const res = await this.post('/auth/signin', { data: values });
+    return this.checkResponse(res);
+  }
+
+  static async getInfo() {
+    const res = await this.get('/auth/user');
+    return this.checkResponse(res);
+  }
+
+  static async logOut() {
+    const res = await this.post('/auth/logout');
+    return this.checkResponse(res);
+  }
+
+  static async findByLogin(login: string) {
+    const res = await this.post('/user/search', { data: { login } });
+    return this.checkResponse(res);
+  }
+}
---
+++ src/types/user.ts
@@ -0,0 +1,20 @@
+export interface ICreateUser {
+  name: string;
+}
+
+export interface ISignInUser {
+  name: string;
+}
+
+export interface IUpdateUserAvatar {
+  name: string;
+  avatar: string;
+}
+
+export interface IUpdateUserInfo {
+  name: string;
+}
+
+export interface IUpdateUserPassword {
+  name: string;
+}
